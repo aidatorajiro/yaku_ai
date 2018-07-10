bf39e7605fece3e9147610b0f346ada760bfa2ab
aidatorajiro <kawarusosu@zoho.com>
2018-07-10 22:13:55 +0900

    add test_simple.py

---
+++ pytorch-CycleGAN-and-pix2pix/test_simple.py
@@ -0,0 +1,28 @@
+import os
+from options.test_options import TestOptions
+from data import CreateDataLoader
+from models import create_model
+from torchvision import utils
+import torch
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()
+    opt.nThreads = 1   # test code only supports nThreads = 1
+    opt.batchSize = 1  # test code only supports batchSize = 1
+    opt.serial_batches = True  # no shuffle
+    opt.no_flip = True  # no flip
+    opt.display_id = -1  # no visdom display
+    data_loader = CreateDataLoader(opt)
+    dataset = data_loader.load_data()
+    model = create_model(opt)
+    model.setup(opt)
+    # create website
+    out_dir = 'results'
+    # test
+    for i, data in enumerate(dataset):
+        if i >= opt.how_many:
+            break
+        model.set_input(data)
+        model.test()
+        visuals = model.get_current_visuals()
+        utils.save_image(torch.cat(list(visuals.values())), os.path.join(out_dir, str(i) + '.png'))
